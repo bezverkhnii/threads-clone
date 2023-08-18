fa6f3a8d9561694c6627190d033cc259bfc950e1
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-08-19 01:09:09 +0200

    implemented backend for threads

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -139,6 +139,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -154,6 +155,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
               <FormControl>
                 <Input type="text" className="account-form_input" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -173,6 +175,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -192,6 +195,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
